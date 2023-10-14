8dbd782dd84effd833a21283044ccf35c66ead34
Thalisson <thalissonpop91@gmail.com>
2023-10-14 04:11:02 -0300

    feat: tela de cadastro

--- src/components/pages/taskform/TaskForm.jsx
+++ src/components/pages/taskform/TaskForm.jsx
@@ -1,4 +1,67 @@
+import { useForm } from 'react-hook-form';
+
+import styles from './TaskForm.module.css';
 
 export default function TaskForm() {
-    return <h1>TaskForm</h1>
+    const { register, handleSubmit, formState: {errors} } = useForm();
+
+    return (
+        <form className={styles.Container} onSubmit={handleSubmit((data) => console.log(data))}>
+            <div className={`${styles.inputForm} ${styles.nivel}`}>
+                <label>Nivel:</label>
+                <select {...register('nivel')}>
+                    <option value="N1" selected>N1</option>
+                    <option value="N2">N2</option>
+                    <option value="N3">N3</option>
+                </select>
+            </div>
+
+            <div className={`${styles.inputForm} ${styles.dual}`}>
+                <div className={styles.composer}>
+                    <label>Protocolo:</label>
+                    <input {...register('protocolo')} />
+                </div>
+
+                <div className={styles.composer}>
+                    <label>Designacão:</label>
+                    <input {...register('designacao')} />
+                </div>
+
+                <div className={styles.composer}>
+                    <label>Previsão:</label>
+                    <input {...register('previsao')} />
+                </div>
+            </div>
+
+            <div className={`${styles.inputForm} ${styles.dual}`}>
+                <div className={styles.composer}>
+                    <label>Atendimento:</label>
+                    <input {...register('atendimento')} />
+                </div>
+
+                <div className={styles.composer}>
+                    <label>Empresa:</label>
+                    <input {...register('empresa')} />
+                </div>
+
+            </div>
+
+            <div className={`${styles.inputForm} ${styles.generic}`}>
+                <label>Filial:</label>
+                <input {...register('filial', { required: true })} />
+                {errors.filial && <p>Informe a Filial</p>}
+            </div>
+
+            <div className={`${styles.inputForm} ${styles.generic}`}>
+                <label>Sintomas:</label>
+                <input {...register('sintomas')} />
+            </div>
+
+            <div className={`${styles.inputForm} ${styles.generic}`}>
+                <label>Motivo:</label>
+                <input {...register('motivo')} />
+            </div>
+            <input className={styles.formSubmit} type='submit' />
+        </form>
+    );
 }
