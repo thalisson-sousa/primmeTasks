094c2d3e5ef8ee491c016ce62bb942d3a2d2ccd2
Thalisson <thalissonpop91@gmail.com>
2023-11-12 10:18:46 -0300

    feat: implements Header

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from './mateusIcon.png';
 
 export default function Header() {
     return (
         <header className={styles.containerHeader}>
-            <img src={logo} />
+            <Link to='/'>
+                <img src={logo} />
+            </Link>
             <div className={styles.containerInputs}>
                 <input type='search' />
                 <input type='submit' />
