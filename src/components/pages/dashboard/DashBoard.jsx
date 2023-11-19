5841d0522aa7c616510d6d395413b4c7284ac4e8
Thalisson <thalissonpop91@gmail.com>
2023-11-19 01:50:32 -0300

    fix: corrigindo bug com 0 chamados

--- src/components/pages/dashboard/DashBoard.jsx
+++ src/components/pages/dashboard/DashBoard.jsx
@@ -11,10 +11,22 @@ export default function DashBoard() {
     return (
         <div className={styles.Container}>
             <ul className={styles.Cards}>
-                <li className={styles.card}>Chamados Abertos: {aberto}</li>
-                <li className={styles.card}>Chamados Andamento: {andamento}</li>
-                <li className={styles.card}>Chamados Concluidos: {concluido}</li>
-                <li className={styles.card}>Chamados Fechados: {fechado}</li>
+                {
+                    aberto != null ? <li className={styles.card}>Chamados Abertos: {aberto}</li>
+                    : <li className={styles.card}>Chamados Abertos: 0</li>
+                }
+                {
+                    andamento != null ? <li className={styles.card}>Chamados Andamento: {andamento}</li>
+                    : <li className={styles.card}>Chamados Andamento: 0</li>
+                }
+                {
+                    concluido != null ? <li className={styles.card}>Chamados Concluidos: {concluido}</li>
+                    : <li className={styles.card}>Chamados Concluidos: 0</li>
+                }
+                {
+                    fechado != null ? <li className={styles.card}>Chamados Fechados: {fechado}</li>
+                    : <li className={styles.card}>Chamados Fechados: 0</li>
+                }
             </ul>
         </div>
     )
